b3c22a2aa0d91abc25bfdef51053bb9671c28d04
Ronak Kothari <ronakkothari301@gmail.com>
2023-11-01 23:51:59 +0530

    adding points system

--- src/Points.jsx
+++ src/Points.jsx
@@ -256,7 +256,7 @@ const Points = () => {
                     <Button
                       onClick={() => {
                         shareOnMobile({
-                          title: `Hello!! You have ${user?.points} Runbhumi Points associated with mobile number -  ${user?.number}`,
+                          title: `Hello ${user?.name}!! You have ${user?.points} Runbhumi Points associated with mobile number -  ${user?.number}`,
                         });
                       }}
                       mt={2}>
