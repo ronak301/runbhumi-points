230223b1b3728568a7e30dee6c5bc07e5aad155f
Ronak Kothari <ronakkothari301@gmail.com>
2024-06-28 14:45:57 +0530

    updating slot

---
+++ src/api/slots.js
@@ -0,0 +1,48 @@
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../firebase";
+import moment from "moment/moment";
+
+export const getAllProperties = async () => {
+  let prop = [];
+  const allPropertiesDocs = query(collection(db, "properties"));
+
+  const querySnapshot = await getDocs(allPropertiesDocs);
+
+  querySnapshot.forEach((doc) => {
+    prop.push({ ...doc.data(), id: doc.id, doc: doc });
+  });
+  return prop;
+};
+
+export const getAllSlots = async (doc) => {
+  let slots = [];
+
+  const slotsCollectionRef = collection(doc.ref, "slots");
+  const slotsQuerySnapshot = await getDocs(slotsCollectionRef);
+
+  slotsQuerySnapshot.forEach((slotDoc) => {
+    slots.push({ ...slotDoc.data(), id: slotDoc.id });
+  });
+
+  slots.sort((a, b) => {
+    return a.sort - b.sort;
+  });
+
+  return slots;
+};
+
+export const getBookedSlotsForDateAndPlayground = async (date, playground) => {
+  let slots = [];
+
+  const slotsCollectionRef = collection(
+    playground.ref,
+    `bookings/${date}/bookedSlot`
+  );
+  const slotsQuerySnapshot = await getDocs(slotsCollectionRef);
+
+  slotsQuerySnapshot.forEach((slotDoc) => {
+    slots.push({ ...slotDoc.data(), id: slotDoc.id });
+  });
+
+  return slots;
+};
