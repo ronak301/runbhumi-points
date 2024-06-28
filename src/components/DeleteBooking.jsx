c29d4162d22757944b849901f321fc5d1b6b964e
Ronak Kothari <ronakkothari301@gmail.com>
2024-06-28 14:48:42 +0530

    adding functionality to delete cooking

---
+++ src/components/DeleteBooking.jsx
@@ -0,0 +1,71 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Box,
+  Button,
+  IconButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+import * as React from "react";
+import { doc, deleteDoc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { deleteSlotBooking } from "../api/bookings";
+
+export function DeleteBooking({ booking, fetchBookings }) {
+  const id = booking?.id;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef();
+
+  const onDeleteEntry = () => {
+    deleteSlotBooking(id, booking?.bookingDate);
+    fetchBookings();
+    onClose();
+  };
+
+  return (
+    <>
+      <Box>
+        <IconButton
+          onClick={() => {
+            onOpen();
+          }}
+          isRound
+          colorScheme="red"
+          aria-label="Delete"
+          icon={<DeleteIcon />}
+        />
+      </Box>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}>
+        <AlertDialogOverlay>
+          <AlertDialogContent mr={4} ml={4}>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Booking
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure you want to delete booking?
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={onDeleteEntry} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  );
+}
